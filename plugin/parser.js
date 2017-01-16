46b5ad60b99005d227d1921a03f356ea82199dbc
wsyscz2 <wsyscz2@hotmail.com>
2017-01-16 00:28:36 -0800

    fix bug for syntax d+2

--- plugin/parser.js
+++ plugin/parser.js
@@ -6,20 +6,35 @@
     var constant = require('./constants.js');
 
     Parser.parse = function(content, rng) {
-        var diceInfo = content.replace(constant.REG_DICE_FORMULA, "<span class='wsy-dice'><b>$2:</b></span>");
-        winston.info("parsed diceInfo:" + diceInfo);
+        var diceInfo = content.replace(constant.REG_DICE_FORMULA, "$2");
+	if (diceInfo.length > 0) {
+	    diceInfo = "<span class='wsy-dice'><b>" + diceInfo + ":</b></span>";
+	}
+        //winston.info("parsed diceInfo:" + diceInfo);
 
         var parsedFormula = "";
         var strFormulas = content.replace(constant.REG_DICE_FORMULA, "$3");
-        winston.info("strFormulas" + strFormulas);
+        //winston.info("strFormulas" + strFormulas);
         var formulas = strFormulas.split(";");
         for (var i = 0; i < formulas.length; ++i) {
             var formula = formulas[i];
-            winston.info("parse formula:" + formula);
+	    if (formula === "") {
+		continue;
+	    }
+            //winston.info("parse formula:" + formula);
             var parsedHead = formula
                 .replace(constant.REG_DICE_CLEAN, "")
-                .replace(/([\+\-])/g, " $1 ");
-            var parsedBody = parsedHead.replace(constant.REG_DICE_DICE, function(dice, number, face){
+                .replace(/([\+\-])/g, " $1 ")
+		.replace(constant.REG_DICE_DICE, function(dice, number, face) {
+		    if (number === "") {
+			number = 1;
+		    }
+		    if (face === "") {
+			face = 20;
+		    }
+		    return number + "d" + face;
+		});
+            var parsedBody = parsedHead.replace(constant.REG_DICE_DICE, function(dice, number, face) {
                 var strRet = "(";
                 for (var i = 0; i < number; ++i) {
                     if (face <= 1) {
@@ -33,12 +48,13 @@
                 }
                 return strRet + ")"
             })
-            var parsedTail = eval(parsedBody.replace(',', '+'));
+	    //winston.info(parsedBody);
+            var parsedTail = eval(parsedBody.replace(/,/g, '+'));
 
             parsedFormula += "<span class='wsy-dice'>" + parsedHead + " ⇒ " + parsedBody + " = " + parsedTail + "</span>";
         }
 
-        winston.info("parsed formulas:" + parsedFormula);
+        //winston.info("parsed formulas:" + parsedFormula);
 
         return diceInfo + parsedFormula;
     };
--- wsy-dice.js
+++ wsy-dice.js
@@ -22,9 +22,9 @@
     //winston.info("load plugin dice");
     Plugin.hooks = {
         parse: function(payload, callback) {
-            winston.info("hooks.parse==================================")
+            //winston.info("hooks.parse==================================")
             //TODO:change roll formula to roll result, use saved random result if there is any
-            winston.info(payload);
+            //winston.info(payload);
 
             var seeds = payload.postData.content.match(constant.REG_DICE_SEED);
             if (seeds != null) {
@@ -38,17 +38,17 @@
             //cut seed tail
             payload.postData.content = payload.postData.content.replace(constant.REG_DICE_SEED, "");
 
-            winston.info(payload);
-            winston.info("=============================================");
+            //winston.info(payload);
+            //winston.info("=============================================");
             callback(null, payload);
         },
         edit: function(payload, callback) {
-            winston.info("hooks.edit===================================");
-            winston.info(payload);
-            winston.info(tempInfo);
+            //winston.info("hooks.edit===================================");
+            //winston.info(payload);
+            //winston.info(tempInfo);
             //add saved random seed to end of the content
             if (tempInfo[payload.post.pid] !== undefined) {
-                winston.info("add seed back");
+                //winston.info("add seed back");
                 payload.post.content += tempInfo[payload.post.pid];
                 tempInfo[payload.post.pid] = undefined;
             } else {
@@ -56,52 +56,52 @@
                 payload.post.content += "\n[diceseed]" + seed + "[/diceseed]";
             }
 
-            winston.info(payload);
-            winston.info("=============================================");
+            //winston.info(payload);
+            //winston.info("=============================================");
             callback(null, payload);
         },
         create: function(payload, callback) {
-            winston.info("hooks.create=================================");
-            winston.info(payload);
+            //winston.info("hooks.create=================================");
+            //winston.info(payload);
             //TODO:add a random seed to end of the content
             var seed = RandomStr();
             payload.post.content += "\n[diceseed]" + seed + "[/diceseed]";
 
-            winston.info("add dice seed");
-            winston.info(payload.post.content);
-            winston.info("=============================================");
+            //winston.info("add dice seed");
+            //winston.info(payload.post.content);
+            //winston.info("=============================================");
             callback(null, payload);
         },
         get: function(payload, callback) {
-            winston.info("hooks.get====================================");
-            winston.info(payload);
+            //winston.info("hooks.get====================================");
+            //winston.info(payload);
             //save the random seed at the end of the content
             var ret = payload.content.match(constant.REG_DICE_SEED);
             if (ret !== null) {
                 tempInfo[payload.pid] = ret[ret.length - 1];
             }
 
-            winston.info(payload);
-            winston.info("=============================================");
+            //winston.info(payload);
+            //winston.info("=============================================");
             callback(null, payload);
         },
         getFields: function(payload, callback) {
-            winston.info("hooks.getFields==============================");
+            //winston.info("hooks.getFields==============================");
             //cut saved random seed at the end of the content
-            winston.info(payload);
+            //winston.info(payload);
 
             for (var i = 0; i < payload.posts.length; ++i) {
                 var data = payload.posts[i];
-                winston.info(data);
+                //winston.info(data);
                 if (data.content !== undefined && data.content !== null) {
-                    winston.info("cut dice seed:" + data.content);
+                    //winston.info("cut dice seed:" + data.content);
                     data.content = data.content.replace(constant.REG_DICE_SEED, "");
                 }
             }
 
-            winston.info(payload);
+            //winston.info(payload);
 
-            winston.info("=============================================");
+            //winston.info("=============================================");
             callback(null, payload);
         }
     };
